d8a97255e8170b57577205cc77f0e07ba646ab65
Jayoxsi <jackstersimmons@gmail.com>
2025-03-17 02:35:14 +0000

    push

--- pack/assets/minecraft/shaders/include/text_effects_config.glsl
+++ pack/assets/minecraft/shaders/include/text_effects_config.glsl
@@ -1,271 +1,271 @@
-TEXT_EFFECT(240, 240, 0) {
+TEXT_EFFECT(240, 240, 0) { // #F0F000
     apply_shaking_movement();
     override_text_color(rgb(255, 82, 82));
     override_shadow_color(rgb(100, 20, 80));
 }
 
-TEXT_EFFECT(240, 240, 4) {
+TEXT_EFFECT(240, 240, 4) { // #F0F004
     apply_waving_movement();
     override_text_color(rgb(255, 235, 60));
     override_shadow_color(rgb(150, 60, 30));
 }
 
-TEXT_EFFECT(240, 240, 8) {
+TEXT_EFFECT(240, 240, 8) { // #F0F008
     apply_iterating_movement();
     override_text_color(rgb(86, 235, 86));
     override_shadow_color(rgb(20, 80, 90));
 }
 
-TEXT_EFFECT(240, 240, 12) {
+TEXT_EFFECT(240, 240, 12) { // #F0F00C
     apply_flipping_movement();
     override_text_color(rgb(74, 222, 209));
     override_shadow_color(rgb(37, 71, 150));
 }
 
-TEXT_EFFECT(240, 240, 16) {
+TEXT_EFFECT(240, 240, 16) { // #F0F010
     apply_skewing_movement();
     override_text_color(rgb(122, 80, 251));
     override_shadow_color(rgb(40, 40, 140));
 }
 
-TEXT_EFFECT(240, 240, 20) {
+TEXT_EFFECT(240, 240, 20) { // #F0F014
     override_text_color(rgb(255, 82, 82));
     apply_outline(rgb(100, 20, 80));
 }
 
-TEXT_EFFECT(240, 240, 24) {
+TEXT_EFFECT(240, 240, 24) { // #F0F018
     apply_gradient(rgb(255, 235, 120), rgb(255, 82, 82));
 }
 
-TEXT_EFFECT(240, 240, 28) {
+TEXT_EFFECT(240, 240, 28) { // #F0F01C
     apply_rainbow();
 }
 
-TEXT_EFFECT(240, 240, 32) {
+TEXT_EFFECT(240, 240, 32) { // #F0F020
     override_text_color(rgb(86, 235, 86));
     override_shadow_color(rgb(20, 80, 90));
     apply_shimmer();
 }
 
-TEXT_EFFECT(240, 240, 36) {
+TEXT_EFFECT(240, 240, 36) { // #F0F024
     override_text_color(rgb(255, 255, 255));
     apply_chromatic_abberation();
     remove_text_shadow();
 }
 
-TEXT_EFFECT(240, 240, 40) {
+TEXT_EFFECT(240, 240, 40) { // #F0F028
     apply_metalic(rgb(160, 160, 200));
 }
 
-TEXT_EFFECT(240, 240, 44) {
+TEXT_EFFECT(240, 240, 44) { // #F0F02C
     override_text_color(rgb(255, 20, 20));
     apply_fire();
 }
 
-TEXT_EFFECT(240, 240, 48) {
+TEXT_EFFECT(240, 240, 48) { // #F0F030
     apply_growing_movement();
     override_text_color(rgb(255, 82, 82));
     override_shadow_color(rgb(100, 20, 80));
 }
 
-TEXT_EFFECT(240, 240, 52) {
+TEXT_EFFECT(240, 240, 52) { // #F0F034
     override_text_color(rgb(255, 235, 60));
     override_shadow_color(rgb(150, 60, 30));
     apply_fade(rgb(86, 235, 86));
 }
 
-TEXT_EFFECT(240, 240, 56) {
+TEXT_EFFECT(240, 240, 56) { // #F0F038
     override_text_color(rgb(86, 235, 86));
     override_shadow_color(rgb(20, 80, 90));
     apply_blinking();
 }
 
-TEXT_EFFECT(240, 240, 60) {
+TEXT_EFFECT(240, 240, 60) { // #F0F03C
     override_text_color(rgb(74, 222, 209));
     override_shadow_color(rgb(37, 71, 150));
     apply_glowing();
 }
 
-TEXT_EFFECT(240, 240, 64) {
+TEXT_EFFECT(240, 240, 64) { // #F0F040
     apply_lesbian_pride();
 }
 
-TEXT_EFFECT(240, 240, 68) {
+TEXT_EFFECT(240, 240, 68) { // #F0F044
     apply_mlm_pride();
 }
 
-TEXT_EFFECT(240, 240, 72) {
+TEXT_EFFECT(240, 240, 72) { // #F0F048
     apply_bisexual_pride();
 }
 
-TEXT_EFFECT(240, 240, 76) {
+TEXT_EFFECT(240, 240, 76) { // #F0F04C
     apply_transgender_pride();
 }
 
-TEXT_EFFECT(240, 240, 80) {
+TEXT_EFFECT(240, 240, 80) { // #F0F050
     apply_pride();
 }
 
-TEXT_EFFECT(240, 240, 84) {
+TEXT_EFFECT(240, 240, 84) { // #F0F054
     apply_pansexual_pride();
 }
 
-TEXT_EFFECT(240, 240, 88) {
+TEXT_EFFECT(240, 240, 88) { // #F0F058
     apply_asexual_pride();
 }
 
-TEXT_EFFECT(240, 240, 92) {
+TEXT_EFFECT(240, 240, 92) { // #F0F05C
     apply_aromantic_pride();
 }
 
-TEXT_EFFECT(240, 240, 96) {
+TEXT_EFFECT(240, 240, 96) { // #F0F060
     apply_non_binary_pride();
 }
 
-TEXT_EFFECT(94, 171, 136) {
+TEXT_EFFECT(94, 171, 136) { // #5EAB88
     apply_waving_movement(1.0, 1.5);
     apply_gradient(rgb(189, 221, 100), rgb(50, 117, 132));
     override_shadow_color(rgb(70, 70, 100));
 }
 
-TEXT_EFFECT(255, 255, 248) {
+TEXT_EFFECT(255, 255, 248) { // #FFFFF8
     apply_vertical_shadow();
     apply_metalic(rgb(255, 255, 255), rgb(150, 163, 177) * 0.95);
     override_shadow_color(rgb(70, 70, 100));
 }
 
-TEXT_EFFECT(240, 240, 100) {
+TEXT_EFFECT(240, 240, 100) { // #F0F064
     override_text_color(rgb(255, 80, 63));
     override_shadow_color(rgb(20, 80, 90));
     apply_shimmer();
 }
 
-TEXT_EFFECT(240, 240, 104) {
+TEXT_EFFECT(240, 240, 104) { // #F0F068
     override_text_color(rgb(129, 189, 230));
     override_shadow_color(rgb(96, 170, 219));
     apply_fade(rgb(200, 136, 252));
 }
 
-TEXT_EFFECT(240, 240, 108) {
+TEXT_EFFECT(240, 240, 108) { // #F0F06C
     apply_waving_movement();
     apply_gradient(rgb(255, 235, 120), rgb(255, 82, 82));
 }
 
-TEXT_EFFECT(240, 240, 112) {
+TEXT_EFFECT(240, 240, 112) { // #F0F070
     override_text_color(rgb(255, 80, 63));
     override_shadow_color(rgb(255, 255, 255));
     apply_shimmer();
 }
 
-TEXT_EFFECT(240, 240, 116) {
-    apply_rainbow();
+TEXT_EFFECT(240, 240, 116) { // #F0F074
+    apply_rainbow(); 
     apply_outline(rgb(255, 255, 255));
 }
 
-TEXT_EFFECT(240, 240, 120) {
+TEXT_EFFECT(240, 240, 120) { // #F0F078
     apply_rainbow();
     apply_outline(rgb(0, 0, 0));
 }
 
-TEXT_EFFECT(240, 240, 124) {
+TEXT_EFFECT(240, 240, 124) { // #F0F07C
     apply_vertical_shadow();
     apply_metalic(rgb(227, 88, 99), rgb(246, 125, 143) * 0.95);
     apply_shimmer();
 }
 
 
-TEXT_EFFECT(240, 240, 128) { // green
+TEXT_EFFECT(240, 240, 128) { // green #F0F080
     apply_animated_gradient_3(rgb(0, 230, 118), rgb(118, 255, 3), rgb(178, 255, 89));
 }
 
-TEXT_EFFECT(240, 240, 132) { // blue
+TEXT_EFFECT(240, 240, 132) { // blue #F0F084
     apply_animated_gradient_3(rgb(37, 70, 232), rgb(37, 135, 232), rgb(76, 205, 245));
 }
 
-TEXT_EFFECT(240, 240, 136) { // yellow
+TEXT_EFFECT(240, 240, 136) { // yellow #F0F082
     apply_animated_gradient_3(rgb(255, 145, 0), rgb(255, 255, 0), rgb(255, 255, 141));
 }
 
-TEXT_EFFECT(240, 240, 140) { // purple
+TEXT_EFFECT(240, 240, 140) { // purple ##F0F08C
     apply_animated_gradient_3(rgb(136, 59, 252), rgb(224, 84, 229), rgb(143, 68, 232));
 }
 
-TEXT_EFFECT(240, 240, 144) { // pink
+TEXT_EFFECT(240, 240, 144) { // pink #F0F090
     apply_animated_gradient_3(rgb(255, 41, 149), rgb(255, 58, 243), rgb(247, 115, 255));
 }
 
-TEXT_EFFECT(240, 240, 148) { // red
+TEXT_EFFECT(240, 240, 148) { // red #F0F094
     apply_animated_gradient_3(rgb(213, 0, 0), rgb(245, 0, 87), rgb(255, 138, 128));
 }
 
-TEXT_EFFECT(240, 240, 152) { // dreamteam
+TEXT_EFFECT(240, 240, 152) { // dreamteam #F0F098
     apply_animated_gradient_3(rgb(168, 131, 243), rgb(131, 147, 249), rgb(222, 126, 246));
 }
 
-TEXT_EFFECT(240, 240, 156) { // cosmicquartz
+TEXT_EFFECT(240, 240, 156) { // cosmicquartz #F0F09C
     apply_animated_gradient_3(rgb(28, 242, 149), rgb(51, 255, 255), rgb(255, 0, 255));
 }
 
-TEXT_EFFECT(240, 240, 160) { // easter
+TEXT_EFFECT(240, 240, 160) { // easter #F0F0A0
     apply_animated_gradient_3(rgb(255, 204, 255), rgb(204, 255, 204), rgb(204, 255, 255));
 }
 
-TEXT_EFFECT(240, 240, 164) {
+TEXT_EFFECT(240, 240, 164) { // #F0F0A4
     apply_pastel_rainbow();
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 168) {
+TEXT_EFFECT(240, 240, 168) { // #F0F0A8
     apply_red_fractal();
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 172) {
+TEXT_EFFECT(240, 240, 172) { // #F0F0AC
     apply_pink_fractal();
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 176) { // icy aurora
+TEXT_EFFECT(240, 240, 176) { // icy aurora #F0F0B0
     apply_animated_gradient_7_smoother(rgb(173, 216, 230), rgb(135, 206, 235), rgb(0, 206, 209), rgb(64, 224, 208), rgb(0, 191, 255), rgb(175, 238, 238), rgb(176, 224, 230));
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 180) { // mint delight
+TEXT_EFFECT(240, 240, 180) { // mint delight #F0F0B4
     apply_animated_gradient_7_smoother(rgb(152, 251, 152), rgb(144, 238, 144), rgb(0, 255, 127), rgb(0, 250, 154), rgb(102, 205, 170), rgb(127, 255, 212), rgb(143, 188, 143));
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 184) { // summer sunshine
+TEXT_EFFECT(240, 240, 184) { // summer sunshine #F0F0B8
     apply_animated_gradient_7_smoother(rgb(255, 14, 14), rgb(253, 108, 26), rgb(255, 148, 50), rgb(255, 204, 24), rgb(255, 246, 39), rgb(255, 118, 60), rgb(231, 59, 59));
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 188) { // wizard magic
+TEXT_EFFECT(240, 240, 188) { // wizard magic #F0F0BC
     apply_animated_gradient_7_smoother(rgb(105, 42, 239), rgb(95, 92, 253), rgb(139, 39, 239), rgb(205, 62, 255), rgb(255, 131, 243), rgb(208, 90, 90), rgb(246, 53, 53));
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 192) { // ocean blue
+TEXT_EFFECT(240, 240, 192) { // ocean blue #F0F0C0
     apply_animated_gradient_7_smoother(rgb(24, 144, 255), rgb(88, 129, 235), rgb(142, 82, 233), rgb(98, 102, 238), rgb(64, 183, 232), rgb(43, 203, 186), rgb(71, 85, 243));
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 196) { // pumpkin patch
+TEXT_EFFECT(240, 240, 196) { // pumpkin patch #F0F0C4
     apply_animated_gradient_7_smoother(rgb(255, 95, 31), rgb(255, 139, 61), rgb(255, 172, 72), rgb(255, 110, 50), rgb(255, 73, 0), rgb(215, 85, 29), rgb(175, 68, 20));
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 200) { // witch brew
+TEXT_EFFECT(240, 240, 200) { // witch brew #F0F0C8
     apply_animated_gradient_7_smoother(rgb(148, 0, 211), rgb(186, 85, 211), rgb(147, 112, 219), rgb(170, 143, 226), rgb(111, 45, 168), rgb(153, 50, 204), rgb(120, 81, 169));
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 204) { // vampire kiss
+TEXT_EFFECT(240, 240, 204) { // vampire kiss #F0F0CC
     apply_animated_gradient_7_smoother(rgb(178, 34, 34), rgb(220, 20, 60), rgb(139, 0, 0), rgb(184, 15, 10), rgb(229, 36, 36), rgb(255, 0, 0), rgb(97, 6, 6));
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 208) { // electric pulse
+TEXT_EFFECT(240, 240, 208) { // electric pulse #F0F0D0
     apply_animated_gradient_7_smoother(rgb(75, 0, 130), rgb(148, 0, 211), rgb(255, 0, 255), rgb(138, 43, 226), rgb(255, 20, 147), rgb(255, 0, 255), rgb(186, 85, 211));
     textData.shouldScale = true;
 }
@@ -275,45 +275,50 @@ TEXT_EFFECT(240, 240, 212) { // spring meadow #F0F0D4
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 216) { // golden sparkle
+TEXT_EFFECT(240, 240, 216) { // golden sparkle #F0F0D8
     apply_animated_gradient_7_smoother(rgb(255, 215, 0), rgb(255, 223, 0), rgb(255, 193, 37), rgb(218, 165, 32), rgb(240, 230, 140), rgb(255, 239, 213), rgb(184, 134, 11));
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 220) { // candy cane
+TEXT_EFFECT(240, 240, 220) { // candy cane #F0F0DC
     apply_animated_gradient_7_smoother(rgb(255, 0, 0), rgb(255, 182, 193), rgb(220, 20, 60), rgb(255, 192, 203), rgb(255, 105, 180), rgb(240, 128, 128), rgb(205, 92, 92));
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 224) { // poinsettia bloom
+TEXT_EFFECT(240, 240, 224) { // poinsettia bloom #F0F0E0 
     apply_animated_gradient_7_smoother(rgb(199, 21, 133), rgb(255, 105, 180), rgb(208, 32, 144), rgb(219, 112, 147), rgb(231, 84, 128), rgb(255, 20, 147), rgb(176, 48, 96));
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 228) { // sweet embrace
+TEXT_EFFECT(240, 240, 228) { // sweet embrace #F0F0E4
     apply_animated_gradient_7_smoother(rgb(255, 182, 193), rgb(255, 160, 122), rgb(255, 105, 180), rgb(255, 69, 105), rgb(219, 112, 147), rgb(255, 218, 185), rgb(250, 128, 114));
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 232) { // enchanted evening
+TEXT_EFFECT(240, 240, 232) { // enchanted evening #F0F0E8
     apply_animated_gradient_7_smoother(rgb(93, 48, 167), rgb(72, 61, 139), rgb(123, 104, 238), rgb(147, 112, 219), rgb(186, 85, 211), rgb(255, 105, 180), rgb(176, 48, 96));
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 236) { // scarlet passion
+TEXT_EFFECT(240, 240, 236) { // scarlet passion #F0F0EC
     apply_character_cycle(rgb(214, 29, 95), rgb(253, 100, 228), 5.0, 0.3); // Love
 }
 
-TEXT_EFFECT(240, 240, 240) { // rainbow fractal
+TEXT_EFFECT(240, 240, 240) { // rainbow fractal #F0F0F0
     apply_rainbow_fractal();
     textData.shouldScale = true;
 }
-TEXT_EFFECT(240, 240, 244) { // pale rainbow fractal
+TEXT_EFFECT(240, 240, 244) { // pale rainbow fractal #F0F0F4
     apply_pale_rainbow_fractal();
     textData.shouldScale = true;
 }
 
-TEXT_EFFECT(240, 240, 248) { // teal ocean
+TEXT_EFFECT(240, 240, 248) { // teal ocean #F0F0F8
     apply_animated_gradient_7_smoother(rgb(0, 191, 255), rgb(135, 206, 235), rgb(64, 224, 208), rgb(102, 205, 170), rgb(102, 205, 170), rgb(144, 238, 144), rgb(0, 255, 127));
+    textData.shouldScale = true;
+}
+
+TEXT_EFFECT(240, 240, 252) { // white ocean #F0F0FC
+    apply_animated_gradient_7_smoother(rgb(0, 191, 255), rgb(135, 206, 235), rgb(182, 226, 255), rgb(222, 242, 255), rgb(182, 226, 255), rgb(135, 206, 235), rgb(0, 191, 255));
     textData.shouldScale = true;
 }
